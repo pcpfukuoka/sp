44ead4702a2b774a4fd1bd4d508a7aa619867306
haramasataka <rorugu@gmail.com>
2012-11-15 12:28:50 +0900

    ＳＰトップ画面
    追加

---
+++ login/top.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>PCP2012</title>
+		<meta name="viewport" content="width=device-width, intital-scale=1">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+	</head>
+
+	<body>
+		<div data-role="page" id="top" align="center">
+			<div data-role="header" data-position="fixed">
+				<div data-role="navbar">
+					<ul>
+						<li><a href="" class="ui-btn-active">スケジュール</a></li>
+						<li><a href="">連絡帳</a></li>
+						<li><a href="">授業</a></li>
+						<li><a href="">成績確認</a></li>
+						<li><a href="">アンケート</a></li>
+					</ul>
+				</div>
+			</div>
+			
+			<div data-role="content">
+			</div>
+			
+			<div data-role="footer" data-position="fixed">
+				<p>PCP2012</p>
+			</div>
+		</div>
+	</body>
+
+</html>
