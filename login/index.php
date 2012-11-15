54a70b82bcfa8de8130023b042cf38dc5e6d3d98
haramasataka <rorugu@gmail.com>
2012-11-15 12:27:57 +0900

    ＳＰログイン画面
    更新

--- login/index.php
+++ login/index.php
@@ -2,30 +2,44 @@
 	session_start();
 	if(isset($_SESSION["login_flg"]) && $_SESSION['login_flg'] == "true")
 	{
-		header("Location: ../index.php");
+		header("Location:top.php");
 	}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title></title>
-</head>
-<body>
-	<div>
-	<?php
-		if(isset($_SESSION['login_flg']) && $_SESSION['login_flg'] == "false")
-		{
-			echo "<h1>ログインに失敗しました。再度ログインしてください。</h1>";
-		}
-		?>
-	
-		<form action="login.php" method="POST">
-			ID：<input type="text" name="id"><br>
-			パスワード：<input type="password" name="pass"><br>
-			<input type="submit" value="ログイン"><br>
-		</form>	
-	</div>
-</body>
+<html lang="ja">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, intital-scale=1">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		
+		<title>PCP2012</title>
+	</head>
+	<body>
+		<div data-role="page" id="login" align="center">
+			<div data-role="header">
+				<p>ログイン画面</p>
+			</div>
+			
+			<div data-role="content">
+				<?php
+					if(isset($_SESSION['login_flg']) && $_SESSION['login_flg'] == "false")
+					{
+						echo "<h1>ログインに失敗しました。再度ログインしてください。</h1>";
+					}
+				?>		
+				<form action="login.php" method="POST">
+					ID：<input type="text" name="id"><br>
+					パスワード：<input type="password" name="pass"><br>
+					<input type="submit" value="ログイン"><br>
+				</form>
+			</div>
+			
+			<div data-role="footer" data-position="fixed">
+				<p>PCP2012</p>
+			</div>
+		</div>
+	</body>
 </html>
