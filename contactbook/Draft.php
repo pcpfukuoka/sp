ee5852cdd5a626166de103cce5edea619c79546b
nisikubo <o-hara1145182@mail.goo.ne.jp>
2012-11-16 12:35:42 +0900

    下書き画面
    更新

--- contactbook/Draft.php
+++ contactbook/Draft.php
@@ -21,13 +21,30 @@
 	Dbdissconnect($dbcon);
 ?>
 
-<html>
+<html lang="ja">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<meta name="viewport" content="width=device-width, intital-scale=1">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 		<title>下書き</title>
 	</head>
 	
 	<body>
+		<div data-role="page" id="Draft" align="center">
+		<div data-role="header" data-position="fixed">
+				<div data-role="navbar">
+					<ul>
+						<li><a href="" class="ui-btn-active">スケジュール</a></li>
+						<li><a href="">連絡帳</a></li>
+						<li><a href="">授業</a></li>
+						<li><a href="">成績確認</a></li>
+						<li><a href="">アンケート</a></li>
+					</ul>
+				</div>
+			</div>
+		<div data-role="content">
 		<div align="center">
 			<font size="7">下書き</font>
 			<br>
@@ -61,5 +78,10 @@
 			</table>
 			<hr>
 		</div>
+		</div>
+	    	
+	    	<div data-role="footer" data-position="fixed">
+				<p>PCP2012</p>
+			</div>
 	</body>
 </html>
