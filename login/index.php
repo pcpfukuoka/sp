6de570a1c5cbbb3fbb563dca69c1cd90527fdb35
haramasataka <rorugu@gmail.com>
2012-11-21 15:14:41 +0900

    ログイン画面
    更新

--- login/index.php
+++ login/index.php
@@ -2,18 +2,19 @@
 	session_start();
 	if(isset($_SESSION["login_flg"]) && $_SESSION['login_flg'] == "true")
 	{
+		header("Location:../index.php");
 	}
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html lang="ja">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 		<meta name="viewport" content="width=device-width, intital-scale=1">
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
-		
+			
 		<title>PCP2012</title>
 	</head>
 	<body>
