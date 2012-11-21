82fe824d0636652589d12316bb552d6c42c99993
nisikubo <o-hara1145182@mail.goo.ne.jp>
2012-11-21 12:39:59 +0900

    下書き
    更新

--- contactbook/Draft.php
+++ contactbook/Draft.php
@@ -21,7 +21,7 @@
 	Dbdissconnect($dbcon);
 ?>
 
-<html lang="ja">
+<html lang="ja"> 
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 		<meta name="viewport" content="width=device-width, intital-scale=1">
