746f5b5b6b6cc61bb3c2ebcaeff6352aa0bc6681
nisikubo <o-hara1145182@mail.goo.ne.jp>
2012-11-22 12:42:26 +0900

    確認画面
    更新

--- contactbook/sendview.php
+++ contactbook/sendview.php
@@ -18,7 +18,7 @@
 	Dbdissconnect($dbcon);
 ?>
        
-<html>
+<html> 
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, intital-scale=1">
