eed3bda0275826a9d58ce1e5fb17d2b6fc9f098b
haramasataka <rorugu@gmail.com>
2012-11-28 10:42:21 +0900

    送信箱画面
    更新

--- contactbook/OutBox.php
+++ contactbook/OutBox.php
@@ -39,7 +39,7 @@
 				<div data-role="navbar">
 					<ul>
 						<li><a href="">スケジュール</a></li>
-						<li><a href="" class="ui-btn-active">連絡帳</a></li>
+						<li><a href="#main.php" class="ui-btn-active">連絡帳</a></li>
 						<li><a href="">授業</a></li>
 						<li><a href="">成績確認</a></li>
 						<li><a href="">アンケート</a></li>
