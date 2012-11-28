cde6ee46b418c9dd1f1afad3ea6bf83796241323
haramasataka <rorugu@gmail.com>
2012-11-28 10:46:46 +0900

    ログイン画面
    更新

--- login/index.php
+++ login/index.php
@@ -10,7 +10,7 @@
 <html lang="ja">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<meta name="viewport" content="width=device-width, intital-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
