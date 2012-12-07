22f84c245ea36e86bb9d1e48b5f6bf4796244399
nisikubo <o-hara1145182@mail.goo.ne.jp>
2012-12-07 12:50:00 +0900

    成績管理
    更新

--- Results_management/Per_list.php
+++ Results_management/Per_list.php
@@ -1,4 +1,4 @@
-<?php
+0p<?php
 
 
 session_start();
@@ -77,95 +77,119 @@ $count_point = mysql_num_rows($result_point);*/
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" ></meta>
+		<meta name="viewport" content="width=device-width, intital-scale=1">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		
 		<title>テスト選択画面</title>
 	</head>
 	
 	<body>
 		<div align = "center">
-			<font size = "6">テスト選択</font><hr><br><br><br>
+		<div data-role="header" data-position="fixed">
+			<div data-role="navbar">
+				<ul>
+					<li><a href="">スケジュール</a></li>
+					<li><a href="../contactbook/main.php">連絡帳</a></li>
+					<li><a href="">授業</a></li>
+					<li><a href="Per_ver.php" class="ui-btn-active">成績確認</a></li>
+					<li><a href="../question/answer_list.php">アンケート</a></li>
+				</ul>
+			</div>
 		</div>
-		
-		<!-- SQLで取り出したテストデータの表示 -->
-		<table border = "1">
-		
-			<tr>
-				<th>日付</th>
-				<th>教科</th>
-				<th>テスト範囲</th>
-				<th>先生</th>
-				<th>点数</th>
-				<th>平均点</th>
-			</tr>
-		
-			<?php 
-			for ($i = 0; $i < $count_test; $i++)
-			{
-				$test = mysql_fetch_array($result_test);
-				
-				$test_seq = $test['test_seq'];
-				
-				$sql = "SELECT point FROM test_result 
-						WHERE test_seq = '$test_seq' 
-						AND user_seq = '$sesID';";
-				$result_point = mysql_query($sql);
-				$point = mysql_fetch_array($result_point);
-				
-				$sql = "SELECT AVG(point) FROM test_result 
-						WHERE test_seq = '$test_seq';";
-				$result_avg = mysql_query($sql);
-				$avg = mysql_fetch_array($result_avg);
-			?>
 			
-			<tr>
-				<td><?= $test['date'] ?></td>
-				<td><?= $test['subject_name'] ?></td>
-				<td><?= $test['contents'] ?></td>
-				<td><?= $test['user_name'] ?></td>
-				
-				<!-- 各生徒の点数を表示させるために、test_seqを持っていく -->
-				<td>
-				<?php 
-				if ($point['point'] == 100)
-					{
-					?>
-						<font size = "5" color = "red">
-						<b><?= $point['point'] ?></b>
-						</font> 
-					<?php 
-					}
-					elseif ($point['point'] >= $avg['AVG(point)'])
-					{
-					?>
-						<font color = "red" >
-						<div align = "right">
-						<?= $point['point'] ?>
-						</div>
-						</font>
-						
-					<?php 
-					}
-					else 
-					{
-					?>
-						<font color = "blue" >
-						<div align = "right">
-						<?= $point['point'] ?>
-						</div>
-						</font>
-					<?php 
-					}?>
-				</td>
-				<td align = "center"><?= $avg['AVG(point)'] ?></td>
-			</tr>
-			<?php
-			}
-			?>
-		</table>
+		<div data-role="content">			
+			<font size = "6">テスト選択</font><hr><br><br><br>
+
+			<!-- SQLで取り出したテストデータの表示 -->
+			<table border = "1">
+			
+				<tr>
+					<th>日付</th>
+					<th>教科</th>
+					<th>テスト範囲</th>
+					<th>先生</th>
+					<th>点数</th>
+					<th>平均点</th>
+				</tr>
 		
-		<?php
-			Dbdissconnect($link);
+				<?php 
+				for ($i = 0; $i < $count_test; $i++)
+				{
+					$test = mysql_fetch_array($result_test);
+					
+					$test_seq = $test['test_seq'];
+					
+					$sql = "SELECT point FROM test_result 
+							WHERE test_seq = '$test_seq' 
+							AND user_seq = '$sesID';";
+					$result_point = mysql_query($sql);
+					$point = mysql_fetch_array($result_point);
+					
+					$sql = "SELECT AVG(point) FROM test_result 
+							WHERE test_seq = '$test_seq';";
+					$result_avg = mysql_query($sql);
+					$avg = mysql_fetch_array($result_avg);
+				?>
+			
+				<tr>
+					<td><?= $test['date'] ?></td>
+					<td><?= $test['subject_name'] ?></td>
+					<td><?= $test['contents'] ?></td>
+					<td><?= $test['user_name'] ?></td>
+					
+					<!-- 各生徒の点数を表示させるために、test_seqを持っていく -->
+					<td>
+					<?php 
+					if ($point['point'] == 100)
+						{
+					?>
+							<font size = "5" color = "red">
+								<b><?= $point['point'] ?></b>
+							</font> 
+					<?php 
+						}
+						elseif ($point['point'] >= $avg['AVG(point)'])
+						{
+					?>
+							<font color = "red" >
+								<?= $point['point'] ?>
+							</font>						
+					<?php 
+						}
+						else 
+						{
+					?>
+							<font color = "blue" >
+								<?= $point['point'] ?>
+							</font>
+					<?php 
+						}
+					?>
+					</td>
+				
+					<td align = "center"><?= $avg['AVG(point)'] ?></td>
+				</tr>
+			
+				<?php
+					}
+				?>
+			</table>
+			
+			<?php
+				Dbdissconnect($link);
 			?>
-		<input type="button" value="戻る" onClick="history.back()">
+			<input type="button" value="戻る" onClick="history.back()">
+			
+		</div>
+	    	 
+	    <div data-role="footer" data-position="fixed" >
+			<p>PCP2012</p>
+			<a href="#" data-rel="back" data-role="button" data-icon="back"  class="ui-btn-left">戻る</a>
+			<a href="../index.php" data-role="button" data-icon="home" data-iconpos="notext" class="ui-btn-right">トップへ</a>
+		</div>
+		</div>
 	</body>
 </html>
 
