c9a81a48fefefe860eadba5aeab825fc771f0f2c
shotaueki <o_hara1145081@mail.goo.ne.jp>
2012-12-18 12:16:03 +0900

    TOPページ
    PC版の左側と同じ情報を表示するように変更

--- index.php
+++ index.php
@@ -4,6 +4,35 @@
 	{
 		header("Location:login/index.php");
 	}
+	
+	$user_seq = $_SESSION['login_info[user]'];
+	$day = date("Y-m-d");
+	require_once("lib/dbconect.php");
+	$dbcon = DbConnect();
+	
+	//各件数を取得
+	//連絡帳の件数
+	$sql = "SELECT new_flg FROM contact_book
+	WHERE new_flg = 1
+	AND contact_book.reception_user_seq = $user_seq;";
+	$result = mysql_query($sql);
+	$cnt_new = mysql_num_rows($result);
+	//プリントの件数
+	$sql = "SELECT * FROM  print_check
+	WHERE user_seq = '$user_seq'
+	AND print_check_flg = 1;";
+	$result = mysql_query($sql);
+	$cnt_print_flg = mysql_num_rows($result);
+	//アンケートの件数
+	$sql = "SELECT * FROM question
+	WHERE question_seq
+	NOT IN ( SELECT question_seq FROM question_awnser WHERE awnser_user_seq = '$user_seq' )
+	AND '" . $day . "' BETWEEN start_date AND end_date";
+	$result = mysql_query($sql);
+	$cnt = mysql_num_rows($result);
+	//データベースを閉じる
+	DBdissconnect($dbcon);
+	
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -31,6 +60,31 @@
 			</div>
 			
 			<div data-role="content">
+			
+						<ul data-role="listview">
+				<?php 
+					if($cnt_new != 0)
+					{?>
+						<li><a href="contactbook/MailBox.php">新着のメッセージが<?= $cnt_new ?>件あります。</a></li>
+				<?php 
+					}
+
+					if($cnt_print_flg != 0)
+					{
+				?>
+						<li><a href="contactbook/MailBox.php">新着のプリントが<?= $cnt_print_flg ?>件あります。</a></li>
+				<?php 
+					}
+					if($cnt != 0)
+					{
+				?>
+						<li><a href="question/answer_list.php">未回答のアンケートが<?= $cnt ?>件あります。</a></li>
+				<?php 
+					}
+					?>
+					
+			</ul>			 
+			
 			</div>
 			
 			<div data-role="footer" data-position="fixed">
